e77d89a5f99407d1a52033c723668466570657d6
mundanecoder <dipankar.laskar45@gmail.com>
2024-05-17 22:54:02 +0530

    learned to load static files, using >> templates >> views >> settings

--- DemoDjango/DemoDjango/views.py
+++ DemoDjango/DemoDjango/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse("hello world")
+    # return HttpResponse("hello world")
+    return render(request,'webApp/index.html')
 
 def about(request):
-    return HttpResponse("Hello from About page")
+    # return HttpResponse("Hello from About page")
+     return render(request,'webApp/about.html')
 
 def contact(request):
     return HttpResponse("Hello from Contact page")
